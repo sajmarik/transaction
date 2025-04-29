1353102ef3f0c3fbec7d7a9815344059ea5cfb9c
safae ajmarik <safaeajmarik@gmail.com>
2025-04-29 17:05:47 +0100

    auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,1 @@
 import './bootstrap';
-
-import './App.jsx';
-//import { createRoot } from 'react-dom/client';
-//import App from './App.jsx';
-
-//const root = createRoot(document.getElementById('app'));
-
-
-//const loadingElement = document.querySelector('.loading-spinner');
-//root.render(<App />);
-//loadingElement?.remove();
